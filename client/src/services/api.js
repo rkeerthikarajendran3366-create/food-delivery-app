792a621886b6107b31d93bf938fb2d4860707433
Keerthika Rajendran <keerthika@example.com>
2026-07-30 20:21:14 +0530

    Update backend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://foodexpress-backend-p9dv.onrender.com/api",
 });
 
 export default API;
